6f2c3342b6106ac09ec48bcaa6d1ac8ca0446e47
Seu Nome <seuemail@example.com>
2024-06-15 01:56:18 -0400

    Esqueci minha senha

--- auth.js
+++ auth.js
@@ -1,12 +1,31 @@
 const { connect } = require("./db")
 
-async function findUser(name) {
+async function findUserByName(name) {
     const connection = await connect();
     return connection
         .collection("users")
         .findOne({ name });
 }
 
+async function findUserByEmail(email) {
+    const connection = await connect();
+    return connection
+        .collection("users")
+        .findOne({ email });
+}
+
+function generatePassword() {
+    const chars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let password = "";
+
+    for (let i = 0; i < 10; i++) {
+        password += chars.charAt(Math.random() * 61)
+    }
+    return password;
+}
+
 module.exports = {
-    findUser
+    findUserByName,
+    findUserByEmail,
+    generatePassword
 }
--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const db = require("../db");
-const { findUser } = require("../auth");
 const bcrypt = require("bcryptjs");
+const auth = require("../auth");
+
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     res.render("login", { title: "Login", message: "" });
 });
 
+router.get('/forgot', function(req, res, next) {
+    res.render("forgot", { title: "Recuperação de Senha", message: "" });
+});
+
+router.post('/forgot', async(req, res, next) => {
+    const email = req.body.email;
+    if (!email)
+        return res.render("forgot", { title: "Recuperação de Senha", message: "O email é obrigatório" });
+
+    const user = await auth.findUserByEmail(email);
+    if (!user)
+        return res.render("forgot", { title: "Recuperação de Senha", message: "O email não está cadastrado" });
+
+    const newPassword = auth.generatePassword();
+    user.password = newPassword;
+
+    await db.updateUser(user._id.toString(), user);
+
+    res.render("forgot", { title: "Recuperação de Senha", message: newPassword });
+});
+
 router.post("/login", async(req, res, next) => {
     const name = req.body.name;
-    const user = await findUser(name);
+    const user = await auth.findUserByName(name);
     if (!user) return res.render("login", { title: "Login", message: "Usuário ou senha inválidos" });
 
     const password = req.body.password;
