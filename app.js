495d5dc83e364fc88661662d8995fc3c451b8086
Seu Nome <seuemail@example.com>
2024-08-04 22:33:37 -0400

    express-session

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require("passport");
 const authMiddleware = require("./authMiddleware");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
@@ -24,7 +26,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 authMiddleware(passport);
+
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_CONNECTION,
+        dbName: process.env.MONGODB_DATABASE,
+        ttl: 30 * 60,
+        autoRemove: "native"
+    }),
+    secret: process.env.MONGO_STORE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 60 * 1000 }
+}))
+
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', loginRouter);
 app.use('/index', indexRouter);
--- authMiddleware.js
+++ authMiddleware.js
@@ -1,26 +1,41 @@
 const auth = require("./auth");
+const db = require("./db");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 
 module.exports = (passport) => {
 
-    passport.use(new LocalStrategy({
-        usernameField: "name",
-        passwordField: "password",
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    })
 
-    }, async(username, password, done) => {
+    passport.deserializeUser(async(id, done) => {
         try {
-            const user = await auth.findUserByName(username);
-            if (!user) return done(null, false);
-
-
-            if (!bcrypt.compareSync(password, user.password))
-                return done(null, false);
-            else
-                return done(null, user);
+            const user = await db.findUser(id)
+            done(null, user);
         } catch (err) {
-            return done(err, false);
+            done(err, false);
         }
-    }));
+
+    })
+
+    passport.use(new LocalStrategy({
+            usernameField: "name",
+            passwordField: "password",
+        },
+        async(username, password, done) => {
+            try {
+                const user = await auth.findUserByName(username);
+                if (!user) return done(null, false);
+
+
+                if (!bcrypt.compareSync(password, user.password))
+                    return done(null, false);
+                else
+                    return done(null, user);
+            } catch (err) {
+                return done(err, false);
+            }
+        }));
 
 }
